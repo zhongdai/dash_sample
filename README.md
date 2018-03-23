46e270b0d2f98aadd698e500fdc357a39e01d4cd
Zhong Dai <zhongdai.au@gmail.com>
2018-03-24 00:05:38 +1100

    improved profiling

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # dash_sample
-A collection of Plotly Dash app
+
+## Profiling Tool
+
+Try to create a online tool to compare the distribution for two different data set (with same attributes), only descrete values are considered.
+
+- run the notebook to create demo data set
+- Run the flask to check the online demo
+
+```bash
+python profiling.py
+```
+
+[linkname](https://youtu.be/YHFgc3eAt80)
--- profiling.py
+++ profiling.py
@@ -2,6 +2,7 @@
 """Profiling Tool Demo"""
 
 import dash
+from plotly import tools
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
@@ -27,8 +28,15 @@ for item in summary_df.columns:
 
 
 app.layout = html.Div([
-    html.H4('Profiling Tool'),
+    dcc.Markdown("""
+## Profiling Tool (Demo)
+This is a profile tool to enable users compare two (or three) data sets, and check the different of distribution on same attributes.
 
+- Select two or three groups
+- Compare against **ONE** attribute, it displays the all values for this attribute on X
+- If select the 2nd attribute, it generates N plots based on the 2nd attribute (N is the unique values of 2nd attribute)
+"""),
+    html.Hr(),
     html.Label('Select Data Group:'),
     dcc.Dropdown(
         id = 'dd-groups',
@@ -36,21 +44,19 @@ app.layout = html.Div([
         value = groups[:2],
         multi = True
     ),
-
     html.Label('Select base attribute:'),
     dcc.Dropdown(
         id = 'dd-attr1',
         options=[ {'label': name, 'value': name} for name in attrs ],
         value=attrs[0]
     ),
-
-    html.Label('Select additional attribute:'),
+    html.Label('Select 2nd attribute:'),
     dcc.Dropdown(
         id = 'dd-attr2',
-        options=[ {'label': name, 'value': name} for name in attrs ],
-        value=attrs[0]
+        options=[{'label':'N/A', 'value':'N/A'}] + [ {'label': name, 'value': name} for name in attrs ],
+        value= 'N/A'
     ),
-
+    html.Hr(),
     dcc.Graph(
         id='my-graph'
     ),
@@ -64,27 +70,70 @@ app.layout = html.Div([
         Input('dd-attr2', 'value')
     ])
 def update_figure(groups, attr1, attr2):
-    traces = []
-    for i in groups:
-        sub_df = summary_df[summary_df['Group'] == i]
-        count_df = sub_df.groupby(attr1).sum()['Count'] / sub_df.sum()['Count'] 
-        traces.append(go.Bar(
-            x=count_df.index,
-            y=count_df.values.tolist(),
-            name=i
-        ))
-    return {
-        'data': traces,
-        'layout': go.Layout(
-            xaxis = {
-                'title':'Values for selected attribute'
-            },
-            yaxis = {
-                'title':"% of Count"
-            },
-            hovermode='closest'
-        )
-    }
+
+    if len(groups) > 3:
+        return html.Div([html.H4('Can not select more than 3 groups')])
+
+    if attr2 != 'N/A':
+        # three colors
+        group_colors = [
+            'rgb(31, 119, 180)',
+            'rgb(255, 127, 14)',
+            'rgb(44, 160, 44)'
+        ]
+        attr2_values = summary_df[attr2].unique()
+        num_of_attr2_values = len(attr2_values)
+        height = num_of_attr2_values * 500
+        sub_plot_titles = ["{} = {}".format(attr2, v) for v in attr2_values]
+
+        fig = tools.make_subplots(rows=num_of_attr2_values, 
+            cols=1, 
+            subplot_titles=tuple(sub_plot_titles))
+
+        for row_id, attr2_v in enumerate(attr2_values, start=1):
+            if row_id == 1:
+                showlegend = True
+            else:
+                showlegend = False
+            row_df = summary_df[summary_df[attr2] == attr2_v] 
+            for group_id, group_value in enumerate(groups, start=1):
+                sub_df = row_df[row_df['Group'] == group_value]
+                count_df = sub_df.groupby(attr1).sum()['Count'] / sub_df.sum()['Count'] 
+                bar_plot = go.Bar(x = count_df.index, 
+                                  y = count_df.values.tolist(), 
+                                  marker=dict( color=group_colors[group_id-1]),
+                                  legendgroup = group_value,
+                                  showlegend = showlegend,
+                                  name = group_value)
+                # run n times, n = num of groups
+                fig.append_trace(bar_plot, row_id, 1)
+        layout = {
+            'height': height,
+            'title': "Compare based on {} and {}".format(attr1, attr2),
+        }
+
+        for i in range(num_of_attr2_values):
+            layout["yaxis{}".format(i+1)] = {'title': '% of Total'}
+            layout["xaxis{}".format(i+1)] = {'title': attr1}
+
+        fig['layout'].update(layout)
+        return fig
+    else:
+        height = 500
+        fig = tools.make_subplots(rows=1, cols=1)
+        for group_value in groups:
+            sub_df = summary_df[summary_df['Group'] == group_value]
+            count_df = sub_df.groupby(attr1).sum()['Count'] / sub_df.sum()['Count'] 
+            bar_plot = go.Bar(x = count_df.index, 
+                              y = count_df.values.tolist(), 
+                              name = group_value)
+                # run n times, n = num of groups
+            fig.append_trace(bar_plot, 1, 1)
+
+        fig['layout'].update(height=height, title="Compare based on {}".format(attr1))  
+        return fig
+
+
 
 app.css.append_css({
     'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
